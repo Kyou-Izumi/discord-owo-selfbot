58b3583e866c93a8807fcfbd8ee1c427321ab1e6
LongAKolangle <thanhlong0975855842@gmail.com>
2023-06-11 14:39:34 +0700

    fixed send command

--- commands/send.js
+++ commands/send.js
@@ -5,12 +5,13 @@ export default {
     callback: async (message, ...args) => {
         try {
             if(message.channel.type == "DM" || message.channel.type == "GROUP_DM") return message.reply("You must send this command in a server")
+            if(!args) return message.reply("You must mention someone and amount of cowoncy")
             const target = message.mentions.members.first()
             const owo = message.guild.members.cache.get(global.owoID)
-            const tool = message.channel.permissionsFor()
             if(!target) return message.reply("You must mention an user to send cowoncy")
             if(!owo) return message.reply("No OwO Bot Found")
-            message.channel.send(`owo send <@!${target.id}>`)
+            if(!args[1].match(/^[0-9]+$/)) return message.reply("You must include an amount of cowoncy to send")
+            message.channel.send(`owo send <@!${target.id}> ${args[1]}`)
             const filter = msg => msg.author.id === global.owoID  && msg.embeds && msg.components.length
             const collector = message.channel.createMessageCollector({filter, max: 5, time: 10_000})
             collector.on("collect", async (m) => {if(m.embeds[0].author.name.includes(message.client.user.tag)) await m.clickButton({ row: 0, col: 0 })})
