cc6d8c7c63124298035d279dc3219a194ef71826
Invaliduser <rainer.schn@outlook.de>
2023-06-19 16:04:54 +0200

    Better run file
    Fixed interrupting after npm i and added error handling

--- run.cmd
+++ run.cmd
@@ -1,4 +1,20 @@
-npm install
-npm update
-node .
+@echo off
+
+echo Installing dependencies...
+call npm i
+IF %ERRORLEVEL% NEQ 0 (
+    echo Failed to install dependencies.
+    exit /b 1
+)
+
+echo Updating dependencies...
+call npm update
+IF %ERRORLEVEL% NEQ 0 (
+    echo Failed to update dependencies.
+    exit /b 1
+)
+
+echo Starting index.js...
+node index.js
+
 pause
